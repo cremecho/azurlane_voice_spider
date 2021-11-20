eb6d0e2fad9e620e8d926be4fffd8251dd3666e6
Cremecho <916783659@qq.com>
2021-11-21 01:39:52 +0800

    init commit

--- blhxwiki/main.py
+++ blhxwiki/main.py
@@ -6,3 +6,11 @@ save_folder = 'C:/Quadratic/azurlane'
 
 if __name__ == '__main__':
     cmdline.execute(['scrapy', 'crawl', 'wikisound'])
+
+
+#
+# `ship_name`中，以列表输入想要爬取的舰船的名字或字符串`'all'`爬取全部舰船语音，支持输入和谐植动物园
+#
+# `save_folder`是保存文件夹,程序将在`save_folder`中以`舰娘名字/语音翻译.mp3`来保存语音
+#
+# `'all'`模式下会记录下载过的舰娘，可以多次分开运行。但不建议用此模式下载，全语音还是解包方便许多。这个项目也只是为了做mod方便看语音内容做的
